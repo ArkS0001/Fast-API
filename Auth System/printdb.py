08a5e804f3ad0610ff60e815aaadadaa9f28ca46
Aakarshit Srivastava <113760964+ArkS0001@users.noreply.github.com>
2024-04-23 23:19:25 +0530

    Create printdb.py

---
+++ Auth System/printdb.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# connect to the database
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+
+# Fetch all users
+sql = "SELECT * FROM `users`;"
+cursor.execute(sql)
+users = cursor.fetchall()
+print("Users:")
+for user in users:
+    print(user)
+
+# Fetch all auth
+sql = "SELECT * FROM `auth`;"
+cursor.execute(sql)
+auths = cursor.fetchall()
+print("Auths:")
+for auth in auths:
+    print(auth)
